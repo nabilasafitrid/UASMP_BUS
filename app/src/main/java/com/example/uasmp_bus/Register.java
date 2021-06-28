bcc19e71b2a491ee75cfcb7a9aaf488cd477e440
nabilasafitrid <abilnabilasafitri@gmail.com>
2021-06-28 12:20:46 +0700

    tambah logo dan intent nama

--- app/src/main/java/com/example/uasmp_bus/Register.java
+++ app/src/main/java/com/example/uasmp_bus/Register.java
@@ -7,20 +7,29 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
     Button register;
+    public TextView get_nama ;
+    public String KEY_NAME ="NAMA";
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
+
+        get_nama = (EditText) findViewById(R.id.nama);
+
         register = findViewById(R.id.register);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                intent.putExtra("NAMA", get_nama.getText().toString());
                 startActivity(intent);
             }
         });
