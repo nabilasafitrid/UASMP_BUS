5efdb92938999749b5b3b1d7ea5eb537487276a4
nabilasafitrid <grepcuy123@gmail.com>
2021-07-05 15:01:38 +0700

    edit2

--- app/src/main/java/com/example/uasmp_bus/Invoice.java
+++ app/src/main/java/com/example/uasmp_bus/Invoice.java
@@ -3,12 +3,54 @@ package com.example.uasmp_bus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
-public class Invoice extends AppCompatActivity {
+public class Invoice extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner metodepembayaran;
+    private String METODE[] = {"BNI","BCA"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invoice);
+
+        // untuk spinner
+        metodepembayaran = findViewById(R.id.metodepembayaran);
+        metodepembayaran.setOnItemSelectedListener(this);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item,METODE);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        metodepembayaran.setAdapter(arrayAdapter);
+
+        //untuk tombol button bayar
+
+        Button tombolbayar = (Button) findViewById(R.id.tombolbayar);
+        tombolbayar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
